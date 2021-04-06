0241613dde4db1ffc911affa93349582ddc422ad
Little-Winge <75264135+Little-Winge@users.noreply.github.com>
2021-04-06 13:42:21 -0400

    Delete commands directory

--- commands/fun/empty-message.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "empty-message",
-    description: "Sends an empty message, literally.",
-    category: "fun",
-    execute(bot, message, args) {
-        message.channel.send("** **")
-    }
-}
--- commands/fun/jew.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "jew",
-    description: "",
-    category: "fun",
-    execute(bot, message, args) {
-        message.channel.send("https://tenor.com/YjsU.gif")
-    }
-}
